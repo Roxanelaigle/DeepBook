ed9b462f7d7bfd67776814d674a81c3dd64124a4
roxanelaigle <roxanelaigle.contact@gmail.com>
2025-03-13 13:04:40 +0100

    changed main_pipeline function in API_package

--- API_package/API_package.py
+++ API_package/API_package.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI, Request
 import json
 import numpy as np
+from main_pipeline.main import main_pipeline
 
 app = FastAPI()
 
@@ -13,7 +14,7 @@ async def predict(request: Request):
     curiosity_level = data['curiosity_level']
     if 'image_array' in data.keys() :
         image_array = np.array(data['image_array'])
-        return full_model(image_array, curiosity_level)
+        return main_pipeline(image_array, curiosity_level)
     else :
         isbn = data['isbn']
-        return full_model(isbn, curiosity_level)
+        return main_pipeline(isbn, curiosity_level)
