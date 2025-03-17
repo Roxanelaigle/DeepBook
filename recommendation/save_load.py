8a6e304085f74c9b9399ba539feaff436c701e0c
Frederic Saudemont <fs1510@gmail.com>
2025-03-17 12:50:35 +0100

    update save_load to take model directory, text_type and len

--- recommendation/save_load.py
+++ recommendation/save_load.py
@@ -3,25 +3,25 @@ from pathlib import Path
 from typing import Tuple
 from transformers import CamembertTokenizer, TFCamembertModel
 
-def save_embeddings(embeddings: np.ndarray, path: Path, n_embeddings: int):
+def save_embeddings(embeddings: np.ndarray, model_dir: Path, text_type: str, n_embeddings: int):
     """Save embeddings to a file."""
-    filename = path
-    path.mkdir(parents=True, exist_ok=True)  # Create directory if it doesn't exist
+    filename = model_dir / f"embeddings_camemBERT_{text_type}_{n_embeddings}_books.npy"  # Construct the full file path
+    filename.parent.mkdir(parents=True, exist_ok=True)  # Create directory if it doesn't exist
     np.save(filename, embeddings)
 
-def load_embeddings(path: Path) -> np.ndarray:
+def load_embeddings(model_dir: Path, text_type: str, n_embeddings: int) -> np.ndarray:
     """Load embeddings from a file."""
-    path = Path(path)
-    if path.exists():
-        return np.load(path)
+    filename = model_dir / f"embeddings_camemBERT_{text_type}_{n_embeddings}_books.npy"  # Construct the full file path
+    if filename.exists():
+        return np.load(filename)
     else:
-        raise FileNotFoundError(f"{path} not found.")
+        raise FileNotFoundError(f"{filename} not found.")
 
 def save_model(model: TFCamembertModel, tokenizer: CamembertTokenizer, path: Path):
     """Save model and tokenizer to a file."""
     path = Path(path)
-    tokenizer.save_pretrained(path)
-    model.save_pretrained(path)
+    tokenizer.save_pretrained(str(path))
+    model.save_pretrained(str(path))
 
 def load_model(path: Path) -> Tuple[TFCamembertModel, CamembertTokenizer]:
     """Load model and tokenizer from a file."""
