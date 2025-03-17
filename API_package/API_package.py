a3577d45c15e5ef40a5406b2bda3a5d27a885526
Frederic Saudemont <fs1510@gmail.com>
2025-03-17 17:41:23 +0100

    plug front to main pipeline

--- API_package/API_package.py
+++ API_package/API_package.py
@@ -6,10 +6,13 @@ from main_pipeline.main import main_pipeline
 import pandas as pd
 from pathlib import Path
 
+ALPHA = 0.2
+EMBEDDINGS_SOURCES = ["titledesc", "genre"]
+
 app = FastAPI()
-app.state.dataset_path = Path("data/VF_data_base_consolidate_clean.csv")
+app.state.dataset_path = Path("raw_data/VF_data_base_consolidate_clean.csv")
 app.state.n_books = pd.read_csv(app.state.dataset_path).shape[0]
-app.state.embeddings_file_path = Path(f"models/camembert_models/embeddings_camemBERT_{app.state.n_books}_books.npy")
+app.state.model_dir = Path(f"models/camembert_models")
 
 @app.post("/")
 async def predict(request: Request):
@@ -20,8 +23,20 @@ async def predict(request: Request):
     print("curiosity_level : " + str(curiosity_level))
     if 'image_array' in data.keys() :
         image_array = np.array(data['image_array'], dtype=np.uint8)
-        return main_pipeline(image_array, curiosity_level,app.state.embeddings_file_path, app.state.dataset_path,n_neighbors=3)
+        return main_pipeline(image_array,
+                             curiosity_level,
+                             app.state.model_dir,
+                             app.state.dataset_path,
+                             n_neighbors=3,
+                             embeddings_sources=EMBEDDINGS_SOURCES,
+                             alpha=ALPHA)
     else :
         isbn = data['isbn']
         print(isbn)
-        return main_pipeline(isbn, curiosity_level, app.state.embeddings_file_path, app.state.dataset_path, n_neighbors=3)
+        return main_pipeline(isbn,
+                             curiosity_level,
+                             app.state.model_dir,
+                             app.state.dataset_path,
+                             n_neighbors=3,
+                             embeddings_sources=EMBEDDINGS_SOURCES,
+                             alpha=ALPHA)
