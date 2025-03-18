c88832de72484e11f5874d5071930570f117107a
Niklas Friese <niklasole.friese@gmail.com>
2025-03-18 15:32:11 +0100

    Finalize Docker setup, clean up Dockerignore and requirements

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
-FROM python:3.11-slim
+FROM python:3.10-slim
+
+RUN apt-get update && apt-get install -y build-essential && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
 
-COPY requirements.txt requirements.txt
+COPY requirements.txt .
+RUN pip install --upgrade pip && pip install -r requirements.txt
 
-RUN pip install --no-cache-dir -r requirements.txt
+COPY recommendation/ ./recommendation/
+COPY models/ ./models/
 
-COPY . .
+# Set Python path so 'recommendation' becomes importable
+ENV PYTHONPATH=/app
 
-EXPOSE 8000
+CMD ["python", "recommendation/main.py"]
 
-CMD ["python", "app.py"]
+COPY raw_data/ ./raw_data/
--- recommendation/save_load.py
+++ recommendation/save_load.py
@@ -1,7 +1,10 @@
+from io import BytesIO
+
 import numpy as np
 from pathlib import Path
 from typing import Tuple
 from transformers import CamembertTokenizer, TFCamembertModel
+from google.cloud import storage
 
 def save_embeddings(embeddings: np.ndarray, model_dir: Path, text_type: str, n_embeddings: int):
     """Save embeddings to a file."""
@@ -12,6 +15,17 @@ def save_embeddings(embeddings: np.ndarray, model_dir: Path, text_type: str, n_e
 def load_embeddings(model_dir: Path, text_type: str, n_embeddings: int) -> np.ndarray:
     """Load embeddings from a file."""
     filename = model_dir / f"embeddings_camemBERT_{text_type}_{n_embeddings}_books.npy"  # Construct the full file path
+
+    # Check if the file exists in the bucket
+    # client = storage.Client()
+    # bucket = client.bucket("le_seau-deepbook")
+    # blob = bucket.blob(str(filename))
+
+    # if blob.exists():
+    #     print(f"Loading embeddings from {filename} in the bucket.")
+    #     return np.load(BytesIO(blob.download_as_bytes()))
+    # Return the embeddings if the file exists in the bucket else check if the file exists locally
+
     if filename.exists():
         return np.load(filename)
     else:
